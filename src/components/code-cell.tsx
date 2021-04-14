6bfb7ed0e45e5a2e040f1476a8fd7af9faaa0f35
heplayskeys <heplayskeys@gmail.com>
2021-04-13 23:57:26 -0700

    Implement redux state in code/text cell and cell list/list item

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from 'react';
+import { useActions } from '../hooks/use-actions';
 import { CodeEditor } from './code-editor';
 import { Preview } from './preview';
 import { bundle } from '../bundler';
 import { Resizable } from './resizable';
+import { Cell } from '../state';
 
-export const CodeCell = () => {
-	const [input, setInput] = useState('');
+interface CodeCellProps {
+	cell: Cell;
+}
+
+// id, type, content
+
+export const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 	const [code, setCode] = useState('');
 	const [err, setErr] = useState('');
+	const { updateCell } = useActions();
 
 	useEffect(() => {
 		const timer = setTimeout(async () => {
-			const output = await bundle(input);
+			const output = await bundle(cell.content);
 			setCode(output.code);
 			setErr(output.err);
 		}, 750);
@@ -19,15 +27,15 @@ export const CodeCell = () => {
 		return () => {
 			clearTimeout(timer);
 		};
-	}, [input]);
+	}, [cell.content]);
 
 	return (
 		<Resizable direction='vertical'>
 			<div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
 				<Resizable direction='horizontal'>
 					<CodeEditor
-						initialValue='const a = 1;'
-						onChange={value => setInput(value)}
+						initialValue={cell.content}
+						onChange={value => updateCell(cell.id, value)}
 					/>
 				</Resizable>
 				<Preview code={code} errorStatus={err} />
